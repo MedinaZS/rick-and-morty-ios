688698244c6e1a396ec953a04a8929e55b46f595
Montse Medina <medinasevilam@gmail.com>
2023-08-23 08:42:52 -0400

    Initial Commit

---
+++ rick-and-morty-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rick-and-morty-ios
+//
+//  Created by Montserrat Medina on 2023-08-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
